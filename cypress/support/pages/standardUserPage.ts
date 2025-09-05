6baaa21ee1501152d6332520de4edd4cc29a33bc
Cleyton Gusmão <cleytongusmao18@gmail.com>
2025-09-04 22:47:39 -0300

    refactor(standardUserPage): rename fansBefore and sabesBefore constants

--- cypress/support/pages/standardUserPage.ts
+++ cypress/support/pages/standardUserPage.ts
@@ -4,21 +4,21 @@ abstract class StandardUserRoles {
 
     static likeHero() {
         cy.get(selectorsStandardUser.fansCount).first().invoke('text').then(text => {
-            const fansAntes = Number(text.trim())   // converte o texto em número e apaga os espaços
+            const fansBefore = Number(text.trim())   // converte o texto em número e apaga os espaços
 
             cy.get(selectorsStandardUser.likeButton).first().click()
-            cy.get(selectorsStandardUser.fansCount).first().should('have.text', String(fansAntes + 1))
+            cy.get(selectorsStandardUser.fansCount).first().should('have.text', String(fansBefore + 1))
         })
     }
 
     static savesHero() {
         cy.get(selectorsStandardUser.savesCount).first().invoke('text').then(text => {
-            const savesAntes = Number(text.trim())
+            const savesBefore = Number(text.trim())
 
             cy.get(selectorsStandardUser.moneyButton).first().click()
             cy.get(selectorsStandardUser.modalConfirm)
             cy.get(selectorsStandardUser.buttonConfirm).click()
-            cy.get(selectorsStandardUser.savesCount).first().should('have.text', String(savesAntes + 1))
+            cy.get(selectorsStandardUser.savesCount).first().should('have.text', String(savesBefore + 1))
         })
     }
 }
